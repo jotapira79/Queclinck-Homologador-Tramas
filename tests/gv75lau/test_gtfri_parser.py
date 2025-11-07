f04635942890922b2fffb1e280c1799d4217a496
jotapira79 <jotapira@gmail.com>
2025-11-07 09:01:24 -0300

    Allow GV75LAU GTFRI reports with empty ext power slot

--- tests/gv75lau/test_gtfri_parser.py
+++ tests/gv75lau/test_gtfri_parser.py
@@ -17,3 +17,16 @@ def test_gtfri_gv75lau_parse_line_resp_y_buff():
         assert data.get("model") == "GV75LAU"
         assert data.get("header") in {"+RESP:GT", "+BUFF:GT"}
         assert data.get("imei") == "866314060583471"
+
+
+def test_gtfri_gv75lau_optional_ext_power_before_report_id():
+    line = (
+        "+RESP:GTFRI,80200C0201,866314062233117,GV75LAU,,50,1,1,0.0,14,605.5,"
+        "-70.638090,-33.433986,20251104153233,0730,0003,9C50,DF0D,01,12,45300.0,,,,,100,210100,,,,20251104153234,0FD9$"
+    )
+
+    data = parse_line(line)
+
+    assert data.get("report_id_type") == "50"
+    assert data.get("number") == 1
+    assert data.get("ext_power_mv") is None
