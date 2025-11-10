46a40c10e8204751e2a19beabe6fe81e8a11de6a
jotapira79 <jotapira@gmail.com>
2025-11-10 10:11:03 -0300

    Add INF expand mask and 1-wire support for GV75LAU GTINF

--- tests/gv75lau/test_gtinf_parser.py
+++ tests/gv75lau/test_gtinf_parser.py
@@ -2,9 +2,12 @@ from queclink import parse_line
 from tests.common.assert_gtinf_shape import assert_gtinf_shape
 
 
+RAW_WITH_ONE_WIRE = "+RESP:GTINF,80200C0100,866314060583471,GV75LAU,11,8956012345678901234,27,0,1,13250,4,4.08,0,2,,20251029091530,0F,01,00,+0000,0,0001,1,0011223344556677,1,7F00,20251029091533,6B91$"
+
 RAW = [
     "+RESP:GTINF,80200C0100,866314060583471,GV75LAU,11,8956012345678901234,27,0,1,13250,4,4.08,0,,,20251029091530,0F,01,00,+0000,0,20251029091533,6B91$",
     "+BUFF:GTINF,80200C0100,866314060583471,GV75LAU,12,8935711001088072340f,38,7,1,27890,,4.11,1,2,,20251007143611,0A,03,01,-0300,1,20251007143612,34DA$",
+    RAW_WITH_ONE_WIRE,
 ]
 
 
@@ -24,3 +27,19 @@ def test_gtinf_gv75lau_prefiere_nombre_cuando_el_prefijo_imei_es_ambiguo():
     data = parse_line(raw)
     assert data["message"] == "GTINF"
     assert data.get("model") == "GV75LAU"
+
+
+def test_gtinf_gv75lau_incluye_mascara_y_datos_one_wire():
+    data = parse_line(RAW_WITH_ONE_WIRE)
+
+    assert data.get("inf_expand_mask") == "0001"
+    assert data.get("one_wire_device_number") == 1
+
+    devices = data.get("one_wire_devices")
+    assert isinstance(devices, list)
+    assert len(devices) == 1
+
+    device = devices[0]
+    assert device.get("one_wire_device_id") == "0011223344556677"
+    assert device.get("one_wire_device_type") == 1
+    assert device.get("one_wire_device_data") == "7F00"
